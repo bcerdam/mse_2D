10229669586a7e220601248972a08d3583d75568
bcerdam <bcmardini@gmail.com>
2023-04-30 17:32:41 -0400

    Correcion y paralelizacion.2

--- core/mse_3D.py
+++ core/mse_3D.py
@@ -5,17 +5,18 @@ import matplotlib.pyplot as plt
 import time
 import seaborn as sns
 
-def plot_arrays(*args, title='', xlabel='', ylabel='', legends=None, save_path=None):
 
+def plot_arrays(data_list, title='', xlabel='', ylabel='', legends=None, save_path=None):
     fig, ax = plt.subplots()
-    ax.set_facecolor('black')
-    ax.grid(True, color='white')
-    for i, arr in enumerate(args):
-        if legends is not None and len(legends) == len(args):
+    plt.grid('black')
+    ax.set_facecolor('lightgrey')
+    for i, data_tuple in enumerate(data_list):
+        name, data = data_tuple
+        if legends is not None and len(legends) == len(data_list):
             label = legends[i]
         else:
-            label = f'{arr}'
-        plt.plot(arr, color=f'C{i}', marker='v', markersize=5, label=label)
+            label = name
+        plt.plot(data, color=f'C{i}', marker='v', markersize=5, label=label, markeredgecolor='black')
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -24,18 +25,23 @@ def plot_arrays(*args, title='', xlabel='', ylabel='', legends=None, save_path=N
         plt.savefig(save_path)
     plt.show()
 
+
+
 # plot_arrays(c_2_0750, p_n_2_0750, w_n_2_0750, xlabel='Scales', ylabel='Entropy', title='MSE 3D (m=2 ; r=0.25*STD)', legends=['Color', 'Pink Noise', 'White Noise'], save_path='/home/bcm/Desktop/Repo/mse_2D/graficos')
 
 
-def read_images_as_numpy(folder_path):
+def read_images_as_numpy(folder_path, f):
     images = []
     for filename in os.listdir(folder_path):
-        if filename.endswith('.png'):
+        if filename.endswith('.png') or filename.endswith('.jpg') or filename.endswith('.jpeg'):
             image_path = os.path.join(folder_path, filename)
             with Image.open(image_path) as img:
                 gray_img = img.convert('L')  # convert to grayscale
                 np_img = np.asarray(gray_img)
-                images.append(np_img)
+                if f == True:
+                    images.append((np_img, filename))
+                else:
+                    images.append(np_img)
     return images
 
 def calculate_mean_matrices(matrices, axis=0):
@@ -51,7 +57,7 @@ def coarse_graining_Z(X, t):
     :param t: escala
     :return: lista coarse-graneada
     '''
-    lista_matrices = read_images_as_numpy(X) # Transforma las imagenes a matrices, devuelve una lista con matrices
+    lista_matrices = read_images_as_numpy(X, f=False) # Transforma las imagenes a matrices, devuelve una lista con matrices
     coarse_list = []
     if t == 1:
         return lista_matrices
@@ -67,17 +73,17 @@ def coarse_graining_Z(X, t):
             coarse_list.append(calculate_mean_matrices(lista_imagenes)) # Calcula el promedio entre "t" imagenes.
     return coarse_list
 
-def d_max(lista_matrices, m, i, j, z, a, b, c):
+def d_max(lista_matrices, m, i, j, k, a, b, c):
     max_dist = 0
     for n in range(m):
-        for k in range(m):
+        for z in range(m):
             for l in range(m):
-                    dist = abs(lista_matrices[z + n][i + k, j + l] - lista_matrices[c + n][a + k, b + l])
+                    dist = abs(lista_matrices[k + n][i + z, j + l] - lista_matrices[c + n][a + z, b + l])
                     if dist > max_dist:
                         max_dist = dist
     return max_dist
 
-def calculate_U_ijz_m(lista_matrices, i, j, z, m, r):
+def calculate_U_ijk_m(lista_matrices, i, j, k, m, r):
     '''
     Calcula la probabilidad de que una ventana (i,j, z) de porte (m*m*m) tenga una diferencia maxima (r) con el resto de las posibles ventanas (a, b, c).
 
@@ -92,17 +98,17 @@ def calculate_U_ijz_m(lista_matrices, i, j, z, m, r):
     count = 0
     H, W = lista_matrices[0].shape
     T = len(lista_matrices)
-    for t in range(1, T-m):
-        for a in range(1, H-m):
-            for b in range(1, W-m):
-                if r == 0:
-                    count += 0
-                elif d_max(lista_matrices, m, i, j, z, a, b, t) <= r:
+    N_m = ((H - m) * (W - m) * (T - m))
+    for c in range(T - m):
+        for a in range(H - m):
+            for b in range(W - m):
+                if c == k and a == i and b == j:
+                    continue
+                if d_max(lista_matrices, m, i, j, k, a, b, c) <= r:
                     count += 1
-    num_windows = ( (H - m) * (W - m) * (T - m) ) - 1
-    return count / num_windows
+    return count / (N_m-1)
 
-def calculate_U_ijz_m_plus_one(lista_matrices, i, j, z, m, r):
+def calculate_U_ijk_m_plus_one(lista_matrices, i, j, k, m, r):
     '''
     Calcula la probabilidad de que una ventana (i,j, z) de porte (m+1*m+1*m+1) tenga una diferencia maxima (r) con el resto de las posibles ventanas (a, b, c).
 
@@ -114,19 +120,18 @@ def calculate_U_ijz_m_plus_one(lista_matrices, i, j, z, m, r):
     :param r: tolerancia de distancia
     :return: devuelve valor de probabilidad U_ijz_m
     '''
-    m += 1
     count = 0
     H, W = lista_matrices[0].shape
     T = len(lista_matrices)
-    for t in range(1, T-m):
-        for a in range(1, H-m):
-            for b in range(1, W-m):
-                if r == 0:
-                    count += 0
-                elif d_max(lista_matrices, m, i, j, z, a, b, t) <= r:
+    N_m = ((H - m) * (W - m) * (T - m))
+    for c in range(T - m):
+        for a in range(H - m):
+            for b in range(W - m):
+                if c == k and a == i and b == j:
+                    continue
+                if d_max(lista_matrices, m, i, j, k, a, b, c) <= r:
                     count += 1
-    num_windows = ( (H - m) * (W - m) * (T - m) ) - 1
-    return count / num_windows
+    return count / (N_m-1)
 
 def calculate_U_m(lista_matrices, m, r):
     """
@@ -136,19 +141,15 @@ def calculate_U_m(lista_matrices, m, r):
     :param r: tolerancia de distancia
     :return: devuelve promedio de todos los U_ijz_m.
     """
-    print("Calculating U_m: ")
     H, W = lista_matrices[0].shape
     T = len(lista_matrices)
-    #U_m = np.zeros((H-m, W-m, T-m))
     U_m = np.array([])
     N_m = (H - m) * (W - m) * (T - m)
-    for z in range(T - m):
-        print(f"U_m time number: {z}")
+    for k in range(T - m):
+        print(f"U_m time number: {k}")
         for i in range(H - m):
-            print(f"U_m row number: {i}")
             for j in range(W - m):
-                # U_m[i, j, z] = calculate_U_ijz_m(lista_matrices, i, j, z, m, r)
-                U_m = np.append(U_m, calculate_U_ijz_m(lista_matrices, i, j, z, m, r))
+                U_m = np.append(U_m, calculate_U_ijk_m(lista_matrices, i, j, k, m, r))
     return np.sum(U_m) / N_m
 
 def calculate_U_m_plus_one(lista_matrices, m, r):
@@ -159,20 +160,17 @@ def calculate_U_m_plus_one(lista_matrices, m, r):
     :param r: tolerancia de distancia
     :return: devuelve promedio de todos los U_ijz_m_plus_one.
     """
-    print("Calculating U_m_plus_one: ")
+    m += 1
     H, W = lista_matrices[0].shape
     T = len(lista_matrices)
-    #U_m = np.zeros((H-m, W-m, T-m))
-    U_m = np.array([])
+    U_m_plus_one = np.array([])
     N_m = (H - m) * (W - m) * (T - m)
-    for z in range(T - m):
-        print(f"U_m_plus_one time number: {z}")
+    for k in range(T - m):
+        print(f"U_m_plus_one time number: {k}")
         for i in range(H - m):
-            print(f"U_m_plus_one row number: {i}")
             for j in range(W - m):
-                #U_m[i, j, z] = calculate_U_ijz_m_plus_one(lista_matrices, i, j, z, m, r)
-                U_m = np.append(U_m, calculate_U_ijz_m_plus_one(lista_matrices, i, j, z, m, r))
-    return np.sum(U_m) / N_m
+                U_m_plus_one = np.append(U_m_plus_one, calculate_U_ijk_m_plus_one(lista_matrices, i, j, k, m, r))
+    return np.sum(U_m_plus_one) / N_m
 
 def calculate_log_ratio(Um, Umplus1):
     """
@@ -190,34 +188,47 @@ def calculate_log_ratio(Um, Umplus1):
 
 def mse_3D(path_imagenes, scales, m, r):
     entropy_values = []
-    cube_std = np.std(np.mean(np.array(coarse_graining_Z(path_imagenes, 1)), axis=0))
+    valores_pixeles = []
+    frames = coarse_graining_Z(path_imagenes, 1)
+    for frame in frames:
+        for rows in frame:
+            for value in rows:
+                if value not in valores_pixeles:
+                    valores_pixeles.append(value)
+    r_parameter = r * np.std(valores_pixeles)
+
     for scale in range(1, scales+1):
+        print(f"Scale: {scale}")
         coarse_grained = coarse_graining_Z(path_imagenes, scale)
-        entropy = calculate_log_ratio(calculate_U_m(coarse_grained, m, r*cube_std), calculate_U_m_plus_one(coarse_grained, m, r*cube_std))
+        entropy = calculate_log_ratio(calculate_U_m(coarse_grained, m, r_parameter),
+                                      calculate_U_m_plus_one(coarse_grained, m, r_parameter))
         entropy_values.append(entropy)
     return np.array(entropy_values)
 
+def parallel_mse_3D(path_imagenes, scale, m, r):
+    valores_pixeles = []
+    frames = coarse_graining_Z(path_imagenes, 1)
+    for frame in frames:
+        for rows in frame:
+            for value in rows:
+                if value not in valores_pixeles:
+                    valores_pixeles.append(value)
+    r_parameter = r * np.std(valores_pixeles)
 
-# Pruebas 100 frames, 10x10 cada una (datos -> 3D)
+    coarse_grained = coarse_graining_Z(path_imagenes, scale)
+    entropy = calculate_log_ratio(calculate_U_m(coarse_grained, m, r_parameter),
+                                  calculate_U_m_plus_one(coarse_grained, m, r_parameter))
+    print(f"Scale: {scale}")
+    return entropy
 
-#####
+# Para probar algoritmo
 
-# start_time = time.time()
-# p_n_1_0750 = mse_3D('/Users/brunocerdamardini/Desktop/repo/mse_2D/datos/3D/pink_noise_frames_10x10', 20, 1, 7.5)
-# end_time = time.time()
-# execution_time_p_n_1_0750 = end_time - start_time
-#
-# #####
+# # White Noise Frames
 #
 # start_time = time.time()
-# w_n_1_0750 = mse_3D('/Users/brunocerdamardini/Desktop/repo/mse_2D/datos/3D/white_noise_frames_10x10', 20, 1, 7.5)
+# w_n_1_050 = mse_3D('/Users/brunocerdamardini/Desktop/repo/mse_2D/datos/3D/white_noise_frames_50x50', 20, 1, 0.5)
 # end_time = time.time()
-# execution_time_w_n_1_0750 = end_time - start_time
-#
-# #####
-#
-# start_time = time.time()
-# c_1_0750 = mse_3D('/Users/brunocerdamardini/Desktop/repo/mse_2D/datos/3D/cte_color_frames_10x10', 20, 1, 7.5)
-# end_time = time.time()
-# execution_time_c_1_0750 = end_time - start_time
+# execution_time_w_n_1_050 = end_time - start_time
+
+
 
